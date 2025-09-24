e8218da31b504976577fcc529f403f774894783b
Thomas Cordingley <tac2218@columbia.edu>
2025-09-24 11:55:36 -0400

    Added photos for each attraction, created all 10 cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>NYC Attractions</h1>
+      <h1>NYC Attractions 🏙 🗽</h1>
       <h2>Welcome! Take a look here at some of the best sights to see during your time in New York.</h2>
       
       
